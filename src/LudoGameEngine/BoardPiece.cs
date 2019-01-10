23ef40df8870a99b014a5ea0bb9f660f4dba2b3e
Samuel Drysén <samuel.drysen@live.se>
2019-01-10 15:35:19 +0100

    Created BoardPiece class
    Created a class that represents a board piece. Each player will in turn have 4 pieces to move around.

---
+++ src/LudoGameEngine/BoardPiece.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LudoGameEngine
+{
+    public class BoardPiece
+    {
+        private int PieceID;
+        private int CurrentPosition;
+
+        public BoardPiece(int arrayPos)
+        {
+            PieceID = arrayPos;
+            CurrentPosition = 0;
+        }
+
+        public void EnterBoard()
+        {
+            CurrentPosition = 1;
+        }
+
+        public void KickFromBoard()
+        {
+
+        }
+
+        public int MovePiece(int diceValue)
+        {
+            CurrentPosition += diceValue;
+
+            return CurrentPosition;
+        }
+    }
+}
